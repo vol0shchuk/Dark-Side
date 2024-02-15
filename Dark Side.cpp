eb86f13b72d4403ac4ee4b4b543493a1d79226b0
vol0shchuk <151207306+vol0shchuk@users.noreply.github.com>
2024-02-15 18:44:36 +0200

    Add files via upload

--- Dark Side.cpp
+++ Dark Side.cpp
@@ -1,6 +1,10 @@
 // Dark Side.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+/*
+
+//завдання 1
+
 #include <iostream>
 #include <string>
 
@@ -51,7 +55,186 @@ int main() {
     cout << "Parrot:" << endl;
     parrot.displayInfo();
     cout << endl;
-}
+}*/
+
+/*
+
+//Завдання 2
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class String {
+protected:
+    char* str;
+    int length;
+
+public:
+    String() : str(nullptr), length(0) {}
+
+    String(const char* cstr) {
+        length = strlen(cstr);
+        str = new char[length + 1];
+        strcpy(str, cstr);
+    }
+
+    String(const String& other) : length(other.length) {
+        str = new char[length + 1];
+        strcpy(str, other.str);
+    }
+
+    String& operator=(const String& other) {
+        if (this != &other) {
+            delete[] str;
+            length = other.length;
+            str = new char[length + 1];
+            strcpy(str, other.str);
+        }
+        return *this;
+    }
+
+    ~String() {
+        delete[] str;
+    }
+
+    int getLength() const {
+        return length;
+    }
+
+    void clear() {
+        delete[] str;
+        str = nullptr;
+        length = 0;
+    }
+
+    String operator+(const String& other) const {
+        String result;
+        result.length = length + other.length;
+        result.str = new char[result.length + 1];
+        strcpy(result.str, str);
+        strcat(result.str, other.str);
+        return result;
+    }
+
+    String& operator+=(const String& other) {
+        *this = *this + other;
+        return *this;
+    }
+
+    bool operator==(const String& other) const {
+        return strcmp(str, other.str) == 0;
+    }
+
+    bool operator!=(const String& other) const {
+        return !(*this == other);
+    }
+
+    const char* getStr() const {
+        return str;
+    }
+};
+
+class BinaryString : public String {
+public:
+    BinaryString() : String() {}
+
+    BinaryString(const char* cstr) {
+        bool valid = true;
+        length = strlen(cstr);
+        for (int i = 0; i < length; i++) {
+            if (cstr[i] != '0' && cstr[i] != '1') {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) {
+            str = new char[length + 1];
+            strcpy(str, cstr);
+        }
+        else {
+            str = nullptr;
+            length = 0;
+        }
+    }
+
+    BinaryString(const String& other) {
+        bool valid = true;
+        for (int i = 0; i < other.getLength(); i++) {
+            if (other.getStr()[i] != '0' && other.getStr()[i] != '1') {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) {
+            length = other.getLength();
+            str = new char[length + 1];
+            strcpy(str, other.getStr());
+        }
+        else {
+            str = nullptr;
+            length = 0;
+        }
+    }
+
+    BinaryString& operator=(const String& other) {
+        BinaryString temp(other);
+        *this = temp;
+        return *this;
+    }
+
+    void changeSign() {
+        for (int i = 0; i < length; i++) {
+            str[i] = (str[i] == '0') ? '1' : '0';
+        }
+    }
+
+    BinaryString operator+(const BinaryString& other) const {
+        BinaryString result;
+        result.length = length + other.length;
+        result.str = new char[result.length + 1];
+        strcpy(result.str, str);
+        strcat(result.str, other.str);
+        return result;
+    }
+
+    BinaryString& operator+=(const BinaryString& other) {
+        *this = *this + other;
+        return *this;
+    }
+
+    bool operator==(const BinaryString& other) const {
+        return strcmp(str, other.str) == 0;
+    }
+
+    bool operator!=(const BinaryString& other) const {
+        return !(*this == other);
+    }
+
+    friend ostream& operator<<(ostream& os, const BinaryString& bs) {
+        os << bs.getStr();
+        return os;
+    }
+};
+
+int main() {
+    String s1("Hello");
+    String s2("World");
+    String s3 = s1 + s2;
+    cout << "s3: " << endl;
+    cout << "Length: " << s3.getLength() << endl;
+
+
+    BinaryString bs1("10101");
+    BinaryString bs2("11010");
+    BinaryString bs3 = bs1 + bs2;
+    cout << "bs3: " << endl;
+    cout << "Length: " << bs3.getLength() << endl;
+    cout << "Content: " << bs3 << endl;
+
+}*/
+
 
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
